478e220ee79ca1802cbe7c298ef7eadfe5f33ec2
GoodayTH <taehyeongkim77@gmail.com>
2022-04-24 22:57:10 +0900

    Part1

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="name">{{name.id}}</div>
+
+  <button class="btn btn-primary" v-on:click="clickedEvent">Click</button>
+</template>
+
+<script>
+import {reactive} from 'vue';
+
+export default {
+  setup() {
+    // const name = ref('Taehyung Kim Code!');
+    const name = reactive({
+       id: 1
+      });
+
+    const fn = () => {
+      return 'HelloWorld';
+    };
+
+    const fn2 = (n) => {
+      return 'HelloWorld2 ' + n;
+    };
+
+    const clickedEvent = () => {
+      name.id = 2;
+    };
+
+    return {
+      name,
+      fn,
+      fn2,
+      clickedEvent
+    };
+  },
+}
+</script>
+
+<style>
+.name{
+  color:#ff0000;
+}
+</style>
