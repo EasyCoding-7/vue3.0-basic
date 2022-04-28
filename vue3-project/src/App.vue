d5b35a4469070ecaa3f6eb168a49679b33e71281
GoodayTH <taehyeongkim77@gmail.com>
2022-04-28 10:04:47 +0900

    Part2-3

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,28 +1,31 @@
 <template>
-    <div class="container">
+  <div class="container">
     <h2>To-Do List</h2>
-    <form  
-      @submit.prevent="onSubmit"
-      class="d-flex"
-    >
-      <div class="flex-grow-1 mr-2">
-        <input
-          class="form-control"
-          type="text" 
-          v-model="todo"
-          placeholder="Type new to-do"
-        >
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <!-- v-model을 통해 양방향 바인딩 -->
+          <input
+            class="form-control"
+            type="text" 
+            v-model="todo"
+            placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button 
+            class="btn btn-primary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div>
-        <button 
-          class="btn btn-primary"
-          type="submit"
-        >
-          Add
-        </button>
+      <!-- 에러처리 -->
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
       </div>
     </form>
-    <!-- 단순 todos 내의 내용을 출력 -->
     <div 
       v-for="todo in todos"
       :key="todo.id"
@@ -45,18 +48,25 @@ export default {
       {id: 1, subject: '휴대폰 사기'},
       {id: 2, subject: '장보기'},
     ]);
+    const hasError = ref(false);
 
     const onSubmit = () => {
-      todos.value.push({
-        id: Date.now(),
-        subject: todo.value
-      });
+      if (todo.value === '') {
+        hasError.value = true;
+      } else {
+        todos.value.push({
+          id: Date.now(),
+          subject: todo.value
+        });
+        hasError.value = false;
+      }
     };
 
     return {
       todo,
       todos,
       onSubmit,
+      hasError,
     };
   },
 }
